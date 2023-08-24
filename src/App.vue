231bb0ab3484712edec92638aae4e572c6ce75b4
kooriookami <bingshuanglingluo@163.com>
2023-08-24 10:52:13 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <el-button>123</el-button>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,15 @@
+import Vue from 'vue';
+import App from './App.vue';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+Vue.config.productionTip = false;
+
+Vue.use(ElementUI);
+
+new Vue({
+  el: '#app',
+  render: h => {
+    return h(App);
+  },
+});
